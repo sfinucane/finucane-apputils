03432c5dc8e4fc3d07f66461008464b65869264e
Sean Anthony Finucane <s.finucane001@gmail.com>
2014-05-28 13:29:26 -0700

    Attempting to fix broken pip. Version 0.2.1
    When installing using pip, ``setup.py`` was complaining about importing
    the python >2.6 compatibility names. This was cruft in the setup file
    anyways.

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python
 """
 """
-# Python 2.6 and newer support
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from future.builtins import (
-                bytes, dict, int, list, object, range, str,
-                ascii, chr, hex, input, next, oct, open,
-                pow, round, super,
-                filter, map, zip)
-try:
-    unicode()
-except NameError:
-    unicode = str
-
-import sys
-__python_version__ = dict()
-try:
-    __python_version__['major'] = sys.version_info.major
-except AttributeError:
-    __python_version__['major'] = sys.version_info[0]
-try:
-    __python_version__['minor'] = sys.version_info.minor
-except AttributeError:
-    __python_version__['minor'] = sys.version_info[1]
-
 from distutils.core import setup
 from pkgutil import walk_packages
 
@@ -43,7 +19,7 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(name='finucane-apputils',
-      version='0.2.0',
+      version='0.2.1',
       description='Finucane Research application framework and utilities for Python',
       long_description=long_description,
       keywords='application framework utilities development finucane',
